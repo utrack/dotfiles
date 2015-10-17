7d8e6ef392e538926e84c2fc404ad2561d3701f5
Nick K <u@colonpipe.biz>
2015-10-17 15:00:16 +0300

    zsh: fixed ^q and ^s, mapped push-line to ^b

--- zsh/config/keys.zsh
+++ zsh/config/keys.zsh
@@ -10,3 +10,9 @@ bindkey '\e[B' down-line-or-beginning-search
 
 # Undo last text change (e.g. completion)
 bindkey '^_' undo
+# Remove push-line,make ^Q work in vim
+stty start '^-' stop '^-'
+bindkey '^B' push-line
+# reclaim ^s as well
+stty stop undef
+stty start undef
