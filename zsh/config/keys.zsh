2e06f5dc577e9ddf7e83f078188ad56827334861
Nick K <u@colonpipe.biz>
2015-10-17 14:46:31 +0300

    zsh: moved from prezto to own cfg

---
+++ zsh/config/keys.zsh
@@ -0,0 +1,12 @@
+# Continue command from history (Up/Down)
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '\eOA' up-line-or-beginning-search
+bindkey '\e[A' up-line-or-beginning-search
+bindkey '\eOB' down-line-or-beginning-search
+bindkey '\e[B' down-line-or-beginning-search
+
+# Undo last text change (e.g. completion)
+bindkey '^_' undo
---
+++ zsh/config/prompt.zsh
@@ -0,0 +1,50 @@
+# prompt
+setopt prompt_subst
+
+# mode-aware arrow
+function p_arrow {
+  if [[ $KEYMAP = "vicmd" ]]; then
+    echo "%F{magenta}»%f"
+  else
+    echo "%F{cyan}»%f"
+  fi
+}
+
+# colored path
+function p_colored_path {
+  local slash="%F{cyan}/%f"
+  echo "${${PWD/#$HOME/~}//\//$slash}"
+}
+
+# anchor, red if last command failed
+function anchor {
+  echo "%(?,%F{cyan},%F{red})λ"
+}
+
+# git info
+function p_vcs {
+  vcs_info
+  echo $vcs_info_msg_0_
+}
+
+# environments:
+#  - ssh
+#  - virtualenv
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+function p_envs {
+  local envs
+  [[ -n $SSH_CLIENT ]]  && envs+="R"
+  [[ -n $VIRTUAL_ENV ]] && envs+="P"
+  [[ -n $envs ]] && echo " %F{green}[%f$envs%F{green}]%f"
+}
+
+# reset prompt on vi-mode/insert mode change
+function zle-line-init zle-keymap-select {
+    zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+PROMPT='
+$(anchor)%f $(p_colored_path)$(p_envs)$(p_vcs)
+$(p_arrow) '
