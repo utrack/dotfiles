52469f2731ab0ea19627b8f74880376fa9cbf216
Nick K <u@colonpipe.biz>
2015-10-27 12:24:03 +0300

    zsh: paths: added cabal path

--- zsh/config/paths.zsh
+++ zsh/config/paths.zsh
@@ -1,5 +1,8 @@
 
 #GOPATH
-export GOPATH=/media/dev/go
+export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 [[ ! -z $GOPATH ]] && export PATH=$PATH:$HOME/.bin:$GOPATH/bin
+
+# cabal
+export PATH=$PATH:~/.cabal/bin
