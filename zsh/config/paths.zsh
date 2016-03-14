c5c9a2fb57118dbee1af28885e09bb6dfe4fc71a
Nick K <u@colonpipe.biz>
2016-03-14 13:09:21 +0300

    Fixed path orders

--- zsh/config/paths.zsh
+++ zsh/config/paths.zsh
@@ -1,8 +1,10 @@
 
 #GOPATH
 export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-[[ ! -z $GOPATH ]] && export PATH=$PATH:$HOME/.bin:$GOPATH/bin
+
+# local binaries
+export PATH=$HOME/.bin:$PATH
+[[ ! -z $GOPATH ]] && export PATH=$GOPATH/bin:$PATH
 
 # cabal
-export PATH=$PATH:~/.cabal/bin
+export PATH=$PATH:$HOME/.cabal/bin
