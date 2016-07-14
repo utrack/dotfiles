6fc72577f7f95ef21f8439002889d866f3138b94
Nikita Koptelov <nikita.koptelov@lazada.com>
2016-07-14 16:55:20 +0300

    bspwm: now adding desktops by relative numbers

--- bin/reset-displays.sh
+++ bin/reset-displays.sh
@@ -2,11 +2,11 @@
 
 if [[ $(xrandr -q | grep " connected" | wc -l) == *2* ]]; then
     xrandr --output DP1 --auto --output eDP1 --auto --below DP1
-    bspc monitor eDP1 -d web term file dev mus
-    bspc monitor DP1 -d im foo foo2 steam
+    bspc monitor ^1 -d web term file dev mus
+    bspc monitor ^2 -d im foo foo2 steam
 else
     xrandr --output DP1 --off --output eDP1 --auto
-    bspc monitor eDP1 -d web term file dev mus im foo foo2 steam
+    bspc monitor ^1 -d web term file dev mus im foo foo2 steam
 fi
 
 notify-send "Displays reset"
