63329f8214fa91dea9068cf9a909ffc369694502
Koptelov Nikita <nkoptelov@ozon.ru>
2018-11-01 14:28:39 +0300

    lst enable ident

--- zsh/.zsh/config/envvars.zsh
+++ zsh/.zsh/config/envvars.zsh
@@ -8,6 +8,6 @@ export VISUAL=vim
 export EDITOR="$VISUAL"
 
 # Java bspwm workarounds
-export _JAVA_OPTIONS='-Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dawt.useSystemAAFontSettings=true' 
+export _JAVA_OPTIONS='-Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dawt.useSystemAAFontSettings=true -Dswing.aatext=true'
 export JAVA_FONTS=/usr/share/fonts/TTF
 export _JAVA_AWT_WM_NONREPARENTING=true
