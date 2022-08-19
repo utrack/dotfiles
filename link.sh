3b95ced23a0ecf25d50606eeb7664ee0c9f1f94d
Koptelov Nikita <nick@koptelov.me>
2022-08-19 17:55:33 +0300

    sync

--- link.sh
+++ link.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 stow bin bspwm compton dunst git i3 spectrwm spacemacs sxhkd tmux vim weechat xinit xresources xscreensaver zsh psd xcompose ulauncher kde
+kwriteconfig5 --file startkderc --group General --key systemdBoot false
+
