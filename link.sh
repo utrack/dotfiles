89be3a1446db634ab63a96064303d32ab704c5dd
Nikita Koptelov <nick@koptelov.me>
2025-09-17 15:00:00 +0200

    cleanup - things I don't use anymore

--- link.sh
+++ link.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-stow bin bspwm compton dunst git i3 spectrwm spacemacs sxhkd tmux vim weechat xinit xresources xscreensaver zsh psd xcompose ulauncher kde
+stow bin dconf doomemacs git gtk i3 kde nnn nushell picom psd psql starship tmux vim xcompose xresources zellij zsh
+# nnn plugins
+sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+# zprezto (custom)
+git submodule update --init --recursive
+# make sure kde starts itself via the script (so i3 can start)
 kwriteconfig5 --file startkderc --group General --key systemdBoot false
 
